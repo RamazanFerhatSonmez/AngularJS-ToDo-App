0a652bdea37abd9cfcad165e2c6ff389ea624948
RamazanFerhat <rmzns1515@gmail.com>
2019-10-14 16:09:54 +0300

    Router oluşturuldu ve home sayfasında işler eklendi

--- ToDoApp/src/app/app.module.ts
+++ ToDoApp/src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {  MatCheckboxModule,
+          MatButtonModule,
+          MatInputModule,
+          MatCardModule, 
+} from '@angular/material';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    DragDropModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', component: NotFoundComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
